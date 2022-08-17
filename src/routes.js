569556f9be0d7e4e25b5d39a697a0cb7a1b194ff
Gropp <fgropp@gmail.com>
2022-08-17 19:08:50 -0300

    get e post

--- src/routes.js
+++ src/routes.js
@@ -20,6 +20,12 @@ routes.post('/users', (request, response) => {
     //localizar o erro
     //console.log(body)
 
+    //validacao para nao incluir um usuario vazio
+    //como a string em javascript é um array, podemos testar o tamanho do array
+    if(name.length < 1) {
+        return response.status(403).json({'mensagem':'Não é possivel criar usuário sem nome'})
+    }
+
     //fazemos um apend no array
     database.push(name)
     return response.status(201).json({'mensagem': `Usuário ${name} criado com sucesso!`})
